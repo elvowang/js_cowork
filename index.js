e23309344613343159ec8138353134f39f43d964
elvo wang <wyingzhu1991@163.com>
2016-08-21 16:27:00 +0800

    jasmine with string

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 console.log("Hello World");
 
-function plus(x, y) {
+var index = {};
+
+index.plus = function(x, y) {
   var sum = x + y;
   return sum;
 }
 
-function minus(x, y) {
+ index.minus = function(x, y) {
   return x - y;
 }
 
-module.exports = {
-  plus: plus,
-  minus: minus
+// function greeting(x) {
+//   if(x=="Elvo") {
+//     var result = "Hello " + x;
+//     return result;
+//   } else {
+//     var result = "Hello Small " + x;
+//     return result;
+//   }
+//
+// }
+index.greeting = function(x) {
+  var result;
+  result = x=="Elvo"? "Hello " + x : "Hello Small " + x;
+  return result;
 }
+
+index.replacing = function(x, y) {
+  y = typeof y == "undefined" ? "world" : y;
+  var result;
+  result = x.replace("nano", y);
+  return result;
+}
+
+index.aHasB = function(x, y) {
+  var result = x.includes(y);
+  return result;
+}
+
+module.exports = index;
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -13,4 +13,31 @@ describe("index spec", function() {
       var result = index.minus(2, 1);
       expect(result).toEqual(1);
     });
+
+    it("", function() {
+      var greetElvo = index.greeting("Elvo");
+      var greetNano = index.greeting("Nano");
+
+      expect(greetElvo).toEqual("Hello Elvo");
+      expect(greetNano).toEqual("Hello Small Nano");
+    });
+
+    it("", function() {
+        var result = index.replacing("hello nano", "elvo");
+        expect(result).toEqual("hello elvo");
+
+        result = index.replacing("hello nano", "elvo1");
+        expect(result).toEqual("hello elvo1");
+
+        result = index.replacing("hello nano");
+        expect(result).toEqual("hello world");
+
+    });
+
+    it("", function() {
+        var result = index.aHasB("hello world", "hello");
+
+        expect(result).toEqual(true);
+    });
+
 })
